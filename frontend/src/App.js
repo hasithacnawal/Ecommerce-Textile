0c3d8f9622003398749c86f933a01a680d617f45
Chana96 <ahasithachandana@gmail.com>
2020-12-05 03:03:17 +0530

    cart init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { BrowserRouter, Route } from "react-router-dom";
+import CartScreen from "./pages/CartScreen";
 import HomeScreen from "./pages/HomeScreen";
 import ProductScreen from "./pages/ProductScreen";
 
@@ -20,6 +21,7 @@ function App() {
           </div>
         </header>
         <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route exact path="/" component={HomeScreen}></Route>
           <Route exact path="/product/:id" component={ProductScreen}></Route>
         </main>
---
+++ frontend/src/pages/CartScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function CartScreen(props) {
+  const productId = props.match.params.id;
+  const qty = props.location.search
+    ? Number(props.location.search.split("=")[1])
+    : 1;
+
+  return (
+    <div>
+      <h1>Add to Cart</h1>
+      <p>
+        ADD TO CART: ProductId: {productId} Qty:{qty}
+      </p>
+    </div>
+  );
+}
+
+export default CartScreen;
