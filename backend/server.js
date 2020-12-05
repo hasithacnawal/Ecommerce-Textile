fc82d6c21d5fbd3dee5dac9de9f58371a1d0df4a
Chana96 <ahasithachandana@gmail.com>
2020-12-05 16:36:11 +0530

    added mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
 import express from "express";
+import mongoose from "mongoose";
+import config from "config";
 import data from "./data.js";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
+
+const db = config.get("mongoURI");
+
+mongoose
+  .connect(db || "mongodb://localhost/amazona", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("mongodb connected"))
+  .catch((err) => console.log(err));
 
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
+
 app.get("/api/products/:id", (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
   if (product) {
@@ -13,6 +28,11 @@ app.get("/api/products/:id", (req, res) => {
   } else {
     res.status(404).send({ message: "Product not found" });
   }
+});
+app.use("/api/users", userRouter);
+
+app.use((err, req, res) => {
+  res.status(500).send({ message: err.message });
 });
 
 app.get("/", (req, res) => {
